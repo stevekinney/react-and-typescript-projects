970420e3d01f204178176ecadb1af827822c6b32
Steve Kinney <hello@stevekinney.net>
2021-04-07 06:47:35 -0500

    Update examples

--- examples/33-nametag-hoc-exercise/src/Application.tsx
+++ examples/33-nametag-hoc-exercise/src/Application.tsx
@@ -1,23 +1,35 @@
-type NameTagProps = {
-  name: string;
-  greeting?: string;
+type UserModel = {
+  accountId: string;
+  displayName: string;
+  isVerified: boolean;
 };
 
-const NameTag = ({ name, greeting = 'Hello' }: NameTagProps) => {
+type NameTagProps = {
+  salutation: string;
+  user: UserModel;
+};
+
+const currentUser = {
+  displayName: 'J Mascis',
+  accountId: '123',
+  isVerified: true
+};
+
+const NameTag = ({ user, salutation }: NameTagProps) => {
   return (
     <main>
       <header>
-        <h1>{greeting}</h1>
+        <h1>{salutation}</h1>
         <p>My Name Is</p>
       </header>
       <section className="display-name">
-        <p>{name}</p>
+        <p>{user.displayName}</p>
       </section>
       <footer />
     </main>
   );
 };
 
-const Application = () => <NameTag name="Steve" greeting="Salutations" />;
+const Application = () => <NameTag salutation="Howdy" user={currentUser} />;
 
 export default Application;
--- examples/34-nametag-hoc-solution/src/Application.tsx
+++ examples/34-nametag-hoc-solution/src/Application.tsx
@@ -1,23 +1,53 @@
-type NameTagProps = {
-  name: string;
-  greeting?: string;
+import { ComponentType } from 'react';
+
+type UserModel = {
+  accountId: string;
+  displayName: string;
+  isVerified: boolean;
 };
 
-const NameTag = ({ name, greeting = 'Hello' }: NameTagProps) => {
+type NameTagProps = {
+  salutation: string;
+  user: UserModel;
+};
+
+const currentUser = {
+  displayName: 'J Mascis',
+  accountId: '123',
+  isVerified: true
+};
+
+type WithUserProps = {
+  user: UserModel;
+};
+
+function withCurrentUser<T extends WithUserProps>(
+  WrappedComponent: ComponentType<T>
+) {
+  const ComponentWithUser = (props: Omit<T, keyof WithUserProps>) => {
+    return <WrappedComponent {...(props as T)} user={currentUser} />;
+  };
+
+  return ComponentWithUser;
+}
+
+const NameTag = ({ user, salutation }: NameTagProps) => {
   return (
     <main>
       <header>
-        <h1>{greeting}</h1>
+        <h1>{salutation}</h1>
         <p>My Name Is</p>
       </header>
       <section className="display-name">
-        <p>{name}</p>
+        <p>{user.displayName}</p>
       </section>
       <footer />
     </main>
   );
 };
 
-const Application = () => <NameTag name="Steve" greeting="Salutations" />;
+const NameTagWithCurrentUser = withCurrentUser(NameTag);
+
+const Application = () => <NameTagWithCurrentUser salutation="Howdy" />;
 
 export default Application;
