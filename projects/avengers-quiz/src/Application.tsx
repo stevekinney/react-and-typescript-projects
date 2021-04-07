dc850bbf8280c36270158871dfdcf7f4dd81f1b6
Steve Kinney <hello@stevekinney.net>
2021-04-07 04:21:20 -0500

    Update Application.tsx

--- projects/avengers-quiz/src/Application.tsx
+++ projects/avengers-quiz/src/Application.tsx
@@ -1,4 +1,5 @@
 import { questions } from './questions';
+import { useState } from 'react';
 
 type QuestionProps = {
   question: string;
@@ -6,14 +7,16 @@ type QuestionProps = {
 };
 
 const Question = ({ question, answer }: QuestionProps) => {
+  const [hidden, toggleHidden] = useState(true);
+
   return (
     <article className="question">
       <header>{question}</header>
       <p className="answer">
-        <span className="blurred">{answer}</span>
+        <span className={`${hidden ? 'blurred' : 'visible'}`}>{answer}</span>
       </p>
       <footer>
-        <button>Toggle Answer</button>
+        <button onClick={() => toggleHidden(!hidden)}>Toggle Answer</button>
       </footer>
     </article>
   );
