79b9a3ed798d6cec8dc2bc9134652746bf506d23
Steve Kinney <hello@stevekinney.net>
2021-04-07 04:08:13 -0500

    Update Application.tsx

--- projects/typing-children/src/Application.tsx
+++ projects/typing-children/src/Application.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 /**
  * Things you could try:
  *
@@ -8,29 +10,28 @@
  * React.ReactChild[];
  */
 
- type BoxProps = { children: any };
+type BoxProps = { children: React.ReactNode; style?: React.CSSProperties };
 
- const Box = ({ children }: BoxProps) => {
-   return (
-     <section style={{ padding: "1em", border: "5px solid purple" }}>
-       {children}
-     </section>
-   );
- };
- 
- export default function Application() {
-   return (
-     <Box>
-       Just a string.
-       <p>Some HTML that is not nested.</p>
-       <Box>
-         <h2>Another React component with one child.</h2>
-       </Box>
-       <Box>
-         <h2>A nested React component with two children.</h2>
-         <p>The second child.</p>
-       </Box>
-     </Box>
-   );
- }
- 
+const Box = ({ children, style = {} }: BoxProps) => {
+  return (
+    <section style={{ padding: '1em', border: '5px solid purple', ...style }}>
+      {children}
+    </section>
+  );
+};
+
+export default function Application() {
+  return (
+    <Box>
+      Just a string.
+      <p>Some HTML that is not nested.</p>
+      <Box style={{ borderColor: 'red' }}>
+        <h2>Another React component with one child.</h2>
+      </Box>
+      <Box>
+        <h2>A nested React component with two children.</h2>
+        <p>The second child.</p>
+      </Box>
+    </Box>
+  );
+}
