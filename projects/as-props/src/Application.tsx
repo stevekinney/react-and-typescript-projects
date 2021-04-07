540eeed49056175b7a1873513a892806d20aa67a
Steve Kinney <hello@stevekinney.net>
2021-04-07 07:41:50 -0500

    Update Application.tsx

--- projects/as-props/src/Application.tsx
+++ projects/as-props/src/Application.tsx
@@ -1,20 +1,33 @@
 import * as React from 'react';
 
-type TextProps = {
+type TextOwnProps<E extends React.ElementType = React.ElementType> = {
   children: string;
+  as?: E;
 };
+
+type TextProps<E extends React.ElementType> = TextOwnProps<E> &
+  Omit<React.ComponentProps<E>, keyof TextOwnProps>;
 
 const exampleText =
   'When I was born, the name for what I was did not exist. They called me nymph, assuming I would be like my mother and aunts and thousand cousins. Least of the lesser goddesses, our powers were so modest they could scarcely ensure our eternities. We spoke to fish and nurtured flowers, coaxed drops from the clouds or salt from the waves. That word, nymph, paced out the length and breadth of our futures.';
 
-function Text({ children }: TextProps) {
-  return <div>{children}</div>;
+const defaultElement = 'div';
+
+function Text<E extends React.ElementType = typeof defaultElement>({
+  children,
+  as,
+  ...rest
+}: TextProps<E>) {
+  const TagName = as || 'div';
+  return <TagName {...rest}>{children}</TagName>;
 }
 
 const Application = () => {
   return (
     <main>
-      <Text>{exampleText}</Text>
+      <Text id="main" as="label" htmlFor="wow">
+        {exampleText}
+      </Text>
     </main>
   );
 };
