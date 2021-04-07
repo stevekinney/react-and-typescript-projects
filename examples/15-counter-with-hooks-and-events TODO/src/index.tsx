bef3a9b34ab1901a9a3dc3980c7a4bebceed93e5
Steve Kinney <hello@stevekinney.net>
2021-04-07 04:38:41 -0500

    Remove old files

--- examples/15-counter-with-hooks-and-events TODO/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from 'react-dom';
-
-import Application from './Application';
-
-import './style.scss';
-
-const rootElement = document.getElementById('root');
-render(<Application />, rootElement);
